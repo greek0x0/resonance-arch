07825313aff20700a4ec95bba674f7e5728a0cea
vicsave <ansi.codex@gmail.com>
2021-12-10 22:19:11 +0000

    (unstable) changes: 	- updated tweaks 	- added configuration files for tweaks (/usr/share/doc/resolinux-tweaks and $HOME/.config/resolinux-tweaks) now im going to have some good sleep.

---
+++ AWM-Resonance-Arch/etc/skel/.config/resolinux-tweaks/config.py
@@ -0,0 +1,19 @@
+# ---- RESOLINUX TWEAKS CONFIG FILE ---- #
+
+# -- THEMES -- #
+#
+# U can add ur themes if u want it.
+# Just remember: wallpapers should be a gif (if TWEAKS_GIF_WALLPAPER = TRUE), and named like "theme_<theme_name>_<a least 1 and max 4>.gif"
+# e.g: theme_cyberpunk_1.gif
+# 
+# Themes are stored in $HOME/.config/awesome/theme-changer
+# Wallpapers are stored in /usr/share/gif
+#
+
+themes = [
+    'cyberpunk',
+    'trains',
+    'default'
+]
+
+TWEAKS_GIF_WALLPAPER = True  # Defines if script need to set animated or static bakground. default - True
