0ef66817d2d31c74ed22d485cb794f018f846e13
vicsave <ansi.codex@gmail.com>
2021-12-11 22:29:21 +0300

    rename main dir

--- AWM-Resonance-Arch/etc/skel/.config/awesome/theme-changer/cyberpunk/cyberpunk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pkill -f background
-
-background auto /usr/share/gif/inspace.gif &
-
-cp -r /home/$USER/.config/awesome/theme-changer/cyberpunk/.local /home/$USER/
--- AWM-Resonance-Arch/etc/skel/.config/awesome/theme-changer/trains/trains.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pkill -f background
-
-background auto /usr/share/gif/train.gif &
-
-cp -r /home/$USER/.config/awesome/theme-changer/trains/.local /home/$USER/
